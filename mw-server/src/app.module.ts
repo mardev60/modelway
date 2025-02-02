332828606f96135290ec4b46c99427dbe43cd6ec
devbu <emmanuelniasse@gmail.com>
2025-02-02 03:11:10 +0100

    remove useless code

--- mw-server/src/app.module.ts
+++ mw-server/src/app.module.ts
@@ -10,11 +10,7 @@ import { ProvidersModule } from './providers/providers.module';
 
 @Module({
   imports: [
-    // MongooseModule.forRoot('mongodb+srv://makiluspn:Uqkdyc8y80BEFe4p@modelway.ofy26.mongodb.net/mw_db?retryWrites=true&w=majority&appName=ModelWay'),
     ConfigModule.forRoot(),
-    // MongooseModule.forRoot(
-    //   'mongodb+srv://emmanuelniasse:fHyfq10zo2A7CaJ6@modelway.ofy26.mongodb.net/mw_db?retryWrites=true&w=majority&appName=ModelWay',
-    // ),
     MongooseModule.forRoot(
       `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_HOST}/${process.env.MONGO_DB}?${process.env.MONGO_OPTIONS}`,
     ),
