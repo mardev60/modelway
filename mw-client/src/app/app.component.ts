0712ea1d36580bae274dd46d7abae06a74a4929e
devbu <emmanuelniasse@gmail.com>
2025-02-06 00:12:51 +0100

    init tailwind

---
+++ mw-client/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
+
+@Component({
+  selector: 'app-root',
+  imports: [NavbarComponent, RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
+  standalone: true,
+})
+export class AppComponent {
+  title = 'client';
+}
