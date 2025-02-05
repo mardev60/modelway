3a7d451ad688df6ce9549207fc28834e4bf66828
devbu <emmanuelniasse@gmail.com>
2025-02-06 00:38:16 +0100

    init pages & suppression generation fichier .scss par défaut

---
+++ mw-client/src/app/pages/backoffice/backoffice.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-backoffice',
+  imports: [],
+  templateUrl: './backoffice.component.html',
+  styleUrl: './backoffice.component.scss'
+})
+export class BackofficeComponent {
+
+}
